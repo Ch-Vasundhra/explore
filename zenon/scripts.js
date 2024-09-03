b2ce7fe9b862020c4e512c5acbb052bdd2c19a91
Vasundhra Chauhan <138037720+Ch-Vasundhra@users.noreply.github.com>
2024-09-03 20:52:07 +0530

    Delete zenon directory

--- zenon/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('taxToggle').addEventListener('change', function () {
-    if (this.checked) {
-        alert('Tax included in the total price');
-    } else {
-        alert('Tax not included in the total price');
-    }
-});
